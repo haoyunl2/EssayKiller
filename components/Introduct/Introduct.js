572c6978a09d56a6edca8c79662473269f2dd3b3
Tigojiang <tigojian@usc.edu>
2021-07-07 17:12:08 +0800

    move collection to component

--- components/Introduct/Introduct.js
+++ components/Introduct/Introduct.js
@@ -14,7 +14,9 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    icon_person:"/image/icon_person.png",
+    icon_rank:"/image/icon_rank.png",
+    univ:"University of California, Berkely"
   },
 
   /**
